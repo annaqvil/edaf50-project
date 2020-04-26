1a92df5f86ff360b22601a177f318c719329ac74
Anna <an2286qv-s@student.lu.se>
2020-04-26 15:09:47 +0200

    Implemented start of interface

--- clientserver/client.cc
+++ clientserver/client.cc
@@ -3,6 +3,10 @@
 #include <stdexcept>
 #include <string>
 #include "connection.h"
+#include "protocol.h"
+#include "messageHandler.h"
+#include <vector>
+#include <sstream>
 
 using std::endl;
 using std::string; 
@@ -11,19 +15,10 @@ using std::cin;
 using std::cerr;
 using std::stoi;
 using std::exception;
+using std::istream; 
+using std::vector; 
+//using std::count; 
 
-void write(const Connection& conn, string ){
-	// do weird shit
-}
-
-string read(const Connection& conn){
-	    string s;
-        char   ch;
-        while ((ch = conn.read()) != '$') {
-                s += ch;
-        }
-        return s;
-}
 
 Connection init(int argc, char* argv[]){
 	if(argc != 3){
@@ -49,14 +44,146 @@ Connection init(int argc, char* argv[]){
 	return conn;
 }
 
+ //all articles have unique numbers
+void printInstructions(){
+	cout << "------------------------------------------------------------------------------------" << endl;
+	cout<< "You can place the following commands: " << endl;
+	cout << "list - lists all newsgroups" << endl <<
+			"list <newsgroup-number> - lists all articles in a specifik newsgroup" << endl <<
+			"read <newsgroup-number> <article-number> - let you read a specific artlice" << endl <<
+			"create <newsgroup-name> - creates a new newsgroup" << endl << 
+			"write - will give you the opportunity to create an article" << endl << 
+			"delete <newsgroup-number> - deletes a specific newsgroup" << endl << 
+			"delete <newsgroup-number> <article-number> - deletes article from specified newsgroup" << endl << 
+			"exit - exits program" << endl << 
+			"help - repets these instuctions" << endl; 
+	cout << "------------------------------------------------------------------------------------" << endl;
+}
+
+void listNewsgoups(){
+	 
+}
+
+void listArticles(int newsgroup){
+	cout << "list articles in newsgroup " << newsgroup << endl;
+}
+
+void readArticle(int newsgroup, int article){
+	cout << "read article nr " << article << " in newsgroup " << newsgroup << endl; 
+}
+
+void createNewsgroup(string newsgroup){
+	cout << "create newsgroup with name: " << newsgroup << endl; 
+}
+
+void writeArticle(){
+	cout << "write article" << endl; 
+}
+
+void deleteNewsgroup(int newsgroup){
+	cout << "delete newsgroup nr " << newsgroup << endl; 
+}
+
+void deleteArticle(int newsgroup, int article){
+	cout << "delete article nr " << article << " from newsgroup " << newsgroup << endl; 
+}
+
 int main(int argc, char* argv[])
 {
 	Connection conn = init(argc, argv); 
+	printInstructions();
 
 	while(true){
-		string message; 
-		cin >> message;
-		cout << message; 
+		cout << "news> ";
+		//allt i följande stycke bör nog brytas ut för minneshantering 
+		string line;
+		std::getline(cin, line); 
+		vector<string> input; 
+		std::istringstream str(line);
+   		std::string word;
+
+   		while ( str >> word ){
+    		input.push_back(word);
+   		}
+
+   		size_t n = input.size();
+
+   		if(n < 1){
+   			continue; 
+   		}
+   		string first = input.at(0); 
+
+   		if(first == "list"){
+   			if(n == 1)
+   				listNewsgoups();
+   			else if(n == 2)
+   				try{
+   					int newsgroup = stoi(input.at(1)); 
+   					listArticles(newsgroup);
+   				} catch (exception& e){
+   					cout << "Wrong input, expected list <newsgroup-number>, e.g. 'list 2'" << endl; 
+   				}
+   			else 
+   				cout << "Expected either 'list' or 'list <newsgroup-number>'. Type help for more help" <<endl; 
+   		}
+   		else if(first == "read"){
+   			if(n == 3){
+				try{
+					int newsgroup = stoi(input.at(1));
+					int article = stoi(input.at(2));
+					readArticle(newsgroup, article); 
+				} catch (exception& e){
+					cout << "Wrong input, expected 'read <newsgroup-number> <article-number>', e.g. 'read 2 3'" << endl; 
+				}
+   			} else {
+   				cout << "Wrong input, expected 'read <newsgroup-number> <article-number>', e.g. 'read 2 3'" << endl;
+   			}
+   		}
+   		else if(first == "create"){
+   			string newsgroup;
+   			if(n==1){
+   				cout << "Wrong input, name of newsgroup needs to be included" << endl; 
+   			} else {
+   				for(int i=1; i<n; i++){
+   					newsgroup += (input.at(i) + " "); 
+   				}
+   				createNewsgroup(newsgroup); 
+   			}
+   		}
+   		else if(first == "delete"){
+   			if(n == 3){
+   				try{
+   					int newsgroup = stoi(input.at(1));
+					int article = stoi(input.at(2));
+					deleteArticle(newsgroup, article); 
+   				} catch (exception& e){
+					cout << "Wrong input, expected 'delete <newsgroup-number> <article-number>', e.g. 'delete 2 3'" << endl; 
+				}
+   			} 
+   			else if(n == 2){
+   				try{
+   					int newsgroup = stoi(input.at(1));
+					deleteNewsgroup(newsgroup); 
+   				} catch (exception& e){
+					cout << "Wrong input, expected 'delete <newsgroup-number>', e.g. 'delete 2'" << endl; 
+				}
+   			} else {
+   				cout << "Wrong input, expected 'delete <newsgroup>' or 'delete <newsgroup> <article>'. Type help for more" << endl; 
+   			}
+   		}
+   		else if(first == "write"){
+   			writeArticle();
+   		}
+   		else if(first == "help"){
+   			printInstructions();
+   		}
+   		else if(first == "exit"){
+   			exit(1); 
+   		}
+   		else{
+   			cout << "Wrong input. Type 'help' to see all options" << endl; 
+   		}
 	}
+
 	return 0;
 }
